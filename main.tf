b856b60da4132c9e01068308018c4054ca63dcf8
Yashar <yashar.rahvar@hmcts.net>
2019-11-07 13:00:20 +0000

    HCL2 expression is not supported with current terraform version.

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "azurerm" {
 locals {
   s2sUrl = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
-  payments_thumbprints_in_quotes     = formatlist("&quot;%s&quot;", var.payments_api_gateway_certificate_thumbprints)
-  payments_thumbprints_in_quotes_str = join(",", local.payments_thumbprints_in_quotes)
+  payments_thumbprints_in_quotes     = "${formatlist("&quot;%s&quot;", var.payments_api_gateway_certificate_thumbprints)}"
+  payments_thumbprints_in_quotes_str = "${join(",", local.payments_thumbprints_in_quotes)}"
 
-  telephony_thumbprints_in_quotes     = formatlist("&quot;%s&quot;", var.telephony_api_gateway_certificate_thumbprints)
-  telephony_thumbprints_in_quotes_str = join(",", local.telephony_thumbprints_in_quotes)
+  telephony_thumbprints_in_quotes     = "${formatlist("&quot;%s&quot;", var.telephony_api_gateway_certificate_thumbprints)}"
+  telephony_thumbprints_in_quotes_str = "${join(",", local.telephony_thumbprints_in_quotes)}"
 
-  bulkscanning_thumbprints_in_quotes     = formatlist("&quot;%s&quot;", var.bulkscanning_api_gateway_certificate_thumbprints)
-  bulkscanning_thumbprints_in_quotes_str = join(",", local.bulkscanning_thumbprints_in_quotes)
+  bulkscanning_thumbprints_in_quotes     = "${formatlist("&quot;%s&quot;", var.bulkscanning_api_gateway_certificate_thumbprints)}"
+  bulkscanning_thumbprints_in_quotes_str = "${join(",", local.bulkscanning_thumbprints_in_quotes)}"
 }
 data "azurerm_key_vault" "payment_key_vault" {
   name                = "ccpay-${var.env}"
