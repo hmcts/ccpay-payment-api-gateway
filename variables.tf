f28333103c17fbb75636ba9c80f69bbf88739b30
aravindan89 <aravindan.ranjan@hmcts.net>
2021-03-25 13:40:37 +0000

    Azure rm upgraded.

--- variables.tf
+++ variables.tf
@@ -17,6 +17,10 @@ variable "env" {
   type = string
 }
 
+variable “common_tags” {
+  type = map(string)
+}
+
 variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
 }
