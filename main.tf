be8be6628b1a40424e4eed56b874bcd39d630278
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-03-31 15:41:50 +0100

    Changes to resolve TF state issue

--- main.tf
+++ main.tf
@@ -39,19 +39,19 @@ data "template_file" "policy_template" {
 data "template_file" "api_template" {
   template = "${file("${path.module}/template/api.json")}"
 }
-resource "azurerm_template_deployment" "api" {
-  template_body       = "${data.template_file.api_template.rendered}"
-  name                = "${var.product}-api-${var.env}"
-  deployment_mode     = "Incremental"
-  resource_group_name = "core-infra-${var.env}"
-  count               = "${var.env != "preview" ? 1: 0}"
+# resource "azurerm_template_deployment" "api" {
+#   template_body       = "${data.template_file.api_template.rendered}"
+#   name                = "${var.product}-api-${var.env}"
+#   deployment_mode     = "Incremental"
+#   resource_group_name = "core-infra-${var.env}"
+#   count               = "${var.env != "preview" ? 1: 0}"
 
-  parameters = {
-    apiManagementServiceName  = "core-api-mgmt-${var.env}"
-    apiName                   = "${var.product}-api"
-    apiProductName            = "${var.product}"
-    serviceUrl                = "http://payment-api-${var.env}.service.core-compute-${var.env}.internal"
-    apiBasePath               = "${local.api_base_path}"
-    policy                    = "${data.template_file.policy_template.rendered}"
-  }
+#   parameters = {
+#     apiManagementServiceName  = "core-api-mgmt-${var.env}"
+#     apiName                   = "${var.product}-api"
+#     apiProductName            = "${var.product}"
+#     serviceUrl                = "http://payment-api-${var.env}.service.core-compute-${var.env}.internal"
+#     apiBasePath               = "${local.api_base_path}"
+#     policy                    = "${data.template_file.policy_template.rendered}"
+#   }
 }
