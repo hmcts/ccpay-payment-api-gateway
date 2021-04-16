edeb4bf3a31cb1e55fa1a836454b12b6e426b5bd
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-04-16 16:03:03 +0100

    Pay 4221 tf import (#38)
    * Commit TF upgrade and import changes
    
    * Add tfvars for ITHC and PerfTest
    
    * Add branches sync config
    
    * Update thumbprints in ithc.tfvars
    
    * Updates

--- main.tf
+++ main.tf
@@ -1,57 +1,67 @@
-provider "azurerm" {
-  version = "1.36.1"
-}
 locals {
-  s2sUrl = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
+  api_mgmt_name     = join("-", ["core-api-mgmt", var.env])
+  api_mgmt_rg       = join("-", ["core-infra", var.env])
+  payment_key_vault = join("-", ["ccpay", var.env])
+  api_base_path     = "payments-api"
+
+  payments_api_url = join("", ["http://payment-api-", var.env, ".service.core-compute-", var.env, ".internal"])
+  s2sUrl           = join("", ["http://rpe-service-auth-provider-", var.env, ".service.core-compute-", var.env, ".internal"])
+
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
-  thumbprints_in_quotes = "${formatlist("&quot;%s&quot;", var.api_gateway_test_certificate_thumbprints)}"
-  thumbprints_in_quotes_str = "${join(",", local.thumbprints_in_quotes)}"
-  api_policy = "${replace(file("template/api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.thumbprints_in_quotes_str)}"
-  api_base_path = "payments-api"
-  dummy = "dummy"
+  thumbprints_in_quotes     = formatlist("&quot;%s&quot;", var.api_gateway_test_certificate_thumbprints)
+  thumbprints_in_quotes_str = join(",", local.thumbprints_in_quotes)
 }
+
 data "azurerm_key_vault" "payment_key_vault" {
-  name = "ccpay-${var.env}"
-  resource_group_name = "ccpay-${var.env}"
+  name                = local.payment_key_vault
+  resource_group_name = local.payment_key_vault
 }
 
 data "azurerm_key_vault_secret" "s2s_client_secret" {
-  name = "gateway-s2s-client-secret"
-  key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
+  name         = "gateway-s2s-client-secret"
+  key_vault_id = data.azurerm_key_vault.payment_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "s2s_client_id" {
-  name = "gateway-s2s-client-id"
-  key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
+  name         = "gateway-s2s-client-id"
+  key_vault_id = data.azurerm_key_vault.payment_key_vault.id
+}
+
+module "api_mgmt_product" {
+  source        = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
+  name          = var.product_name
+  api_mgmt_name = local.api_mgmt_name
+  api_mgmt_rg   = local.api_mgmt_rg
+}
+
+module "api_mgmt_api" {
+  source        = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
+  name          = join("-", [var.product_name, "api"])
+  display_name  = "Payments API"
+  api_mgmt_name = local.api_mgmt_name
+  api_mgmt_rg   = local.api_mgmt_rg
+  product_id    = module.api_mgmt_product.product_id
+  path          = local.api_base_path
+  service_url   = local.payments_api_url
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.payments.json"
+  revision      = "1"
 }
 
 data "template_file" "policy_template" {
-  template = "${file("${path.module}/template/api-policy.xml")}"
+  template = file(join("", [path.module, "/template/api-policy.xml"]))
 
-  vars {
-    allowed_certificate_thumbprints = "${local.thumbprints_in_quotes_str}"
-    s2s_client_id = "${data.azurerm_key_vault_secret.s2s_client_id.value}"
-    s2s_client_secret = "${data.azurerm_key_vault_secret.s2s_client_secret.value}"
-    s2s_base_url = "${local.s2sUrl}"
+  vars = {
+    allowed_certificate_thumbprints = local.thumbprints_in_quotes_str
+    s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
+    s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
+    s2s_base_url                    = local.s2sUrl
   }
 }
 
-data "template_file" "api_template" {
-  template = "${file("${path.module}/template/api.json")}"
-}
-resource "azurerm_template_deployment" "api" {
-  template_body       = "${data.template_file.api_template.rendered}"
-  name                = "${var.product}-api-${var.env}"
-  deployment_mode     = "Incremental"
-  resource_group_name = "core-infra-${var.env}"
-  count               = "${var.env != "preview" ? 1: 0}"
-
-  parameters = {
-    apiManagementServiceName  = "core-api-mgmt-${var.env}"
-    apiName                   = "${var.product}-api"
-    apiProductName            = "${var.product}"
-    serviceUrl                = "http://payment-api-${var.env}.service.core-compute-${var.env}.internal"
-    apiBasePath               = "${local.api_base_path}"
-    policy                    = "${data.template_file.policy_template.rendered}"
-  }
+module "api_mgmt_policy" {
+  source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+  api_mgmt_name          = local.api_mgmt_name
+  api_mgmt_rg            = local.api_mgmt_rg
+  api_name               = module.api_mgmt_api.name
+  api_policy_xml_content = data.template_file.policy_template.rendered
 }
