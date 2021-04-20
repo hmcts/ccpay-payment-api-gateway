ad2efe6356fdb07f4245eb27bbb8af8770aaa1b8
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-04-20 11:52:37 +0100

    Uncomment TF script

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-# this variable will be accessible to tests as API_GATEWAY_URL environment variable
-# output "api_gateway_url" {
-#   value = join("", ["https://", local.api_mgmt_name, ".azure-api.net/", local.api_base_path])
-# }
+this variable will be accessible to tests as API_GATEWAY_URL environment variable
+output "api_gateway_url" {
+  value = join("", ["https://", local.api_mgmt_name, ".azure-api.net/", local.api_base_path])
+}
