334c18ef110a6f0e0edc34717496d5b34fe14bff
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-07-13 18:22:04 +0100

    Encoding fix (#45)
    * Adding Approval required
    
    * Changes to resolve TF state issue
    
    * Commented template_file
    
    * Test
    
    * Add remote backend
    
    * Add common-tags variable
    
    * Created new var to retain product name as payments
    
    * Comment TF config to create empty state file
    
    * Uncomment TF script
    
    * Fix for synatax issues
    
    * Pointing new end point for performance fix
    
    * aad-new-thumbprint-to-aat
    
    Adding new thumbprint to aat for testing
    
    * Remove character encoding
    
    * Re-instate state file reference
    
    * Add new thumprint for ITHC
    
    * Removed duplicate var
    
    Co-authored-by: AyanDuttaHMCTS <53818530+AyanDuttaHMCTS@users.noreply.github.com>
    Co-authored-by: Yashar Rahvar <Yasharrah909@gmail.com>
    Co-authored-by: krishnanuthalapati <32389208+krishnanuthalapati@users.noreply.github.com>
    Co-authored-by: Aravindan Rajan <aravindan.ranjan@hmcts.net>

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ locals {
   s2sUrl           = join("", ["http://rpe-service-auth-provider-", var.env, ".service.core-compute-", var.env, ".internal"])
 
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
-  thumbprints_in_quotes     = formatlist("&quot;%s&quot;", var.api_gateway_test_certificate_thumbprints)
+  thumbprints_in_quotes     = formatlist("\"%s\"", var.api_gateway_test_certificate_thumbprints)
   thumbprints_in_quotes_str = join(",", local.thumbprints_in_quotes)
 }
 
@@ -43,7 +43,7 @@ module "api_mgmt_api" {
   product_id    = module.api_mgmt_product.product_id
   path          = local.api_base_path
   service_url   = local.payments_api_url
-  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.payments.json"
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.recon-payments.json"
   revision      = "1"
 }
 
