0af31bcb71f6ee8cea59c513190ec32a8c225290
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-30 13:44:40 +0000

    Update alert_action_group.tf

--- alert_action_group.tf
+++ alert_action_group.tf
@@ -12,11 +12,11 @@ module "alert-action-group" {
   action_group_name      = "Bulk Print Alert (${var.env})"
   short_name             = "BSP_Alert"
   email_receiver_name    = "BSP Alerts And Monitoring"
-  email_receiver_address = data.azurerm_key_vault_secret.source_bsp_email_secret.value
+  email_receiver_address = "anshika.nigam@hmcts.net"
 }
 
 resource "azurerm_key_vault_secret" "alert_action_group_name" {
-  key_vault_id = module.send-letter-key-vault.key_vault_id
+  #key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "alert-action-group-name"
   value        = module.alert-action-group.action_group_name
 }
