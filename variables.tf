866befff5ab58e2220d909a4cca40e6f0ecb6d35
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-30 13:39:04 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,6 +16,9 @@ variable "env" {
   type = string
 }
 
+variable "component" {
+}
+
 variable "tenant_id" {
   type        = string
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
