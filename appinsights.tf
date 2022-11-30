5e23dbb35da9e5b40bff34ea89ab5cd19d98cb3c
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-30 13:43:02 +0000

    Update appinsights.tf

--- appinsights.tf
+++ appinsights.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
-  key_vault_id = module.send-letter-key-vault.key_vault_id
+  #key_vault_id = module.send-letter-key-vault.key_vault_id
 }
 
 resource "azurerm_application_insights" "appinsights" {
