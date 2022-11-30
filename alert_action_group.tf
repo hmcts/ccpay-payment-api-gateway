f0c93edca3b5f54a24bf6a2824148c4ed2275773
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-11-30 13:40:24 +0000

    Create alert_action_group.tf

---
+++ alert_action_group.tf
@@ -0,0 +1,26 @@
+data "azurerm_key_vault_secret" "source_bsp_email_secret" {
+  key_vault_id = module.send-letter-key-vault.key_vault_id
+  name         = "send-letter-alert-email"
+}
+
+module "alert-action-group" {
+  source   = "git@github.com:hmcts/cnp-module-action-group"
+  location = "global"
+  env      = var.env
+
+  resourcegroup_name     = local.api_mgmt_rg
+  action_group_name      = "Bulk Print Alert (${var.env})"
+  short_name             = "BSP_Alert"
+  email_receiver_name    = "BSP Alerts And Monitoring"
+  email_receiver_address = data.azurerm_key_vault_secret.source_bsp_email_secret.value
+}
+
+resource "azurerm_key_vault_secret" "alert_action_group_name" {
+  key_vault_id = module.send-letter-key-vault.key_vault_id
+  name         = "alert-action-group-name"
+  value        = module.alert-action-group.action_group_name
+}
+
+output "action_group_name" {
+  value = module.alert-action-group.action_group_name
+}
