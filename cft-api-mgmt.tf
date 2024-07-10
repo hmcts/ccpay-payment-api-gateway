acf30876f3d54b16f2f2a5b119779f4b0cec04d0
David Jones <dave.jones@hmcts.net>
2024-07-10 17:12:08 +0100

    PAY-6602: Adding developer access.

--- cft-api-mgmt.tf
+++ cft-api-mgmt.tf
@@ -15,6 +15,7 @@ module "cft_api_mgmt_product" {
   name          = var.product_name
   api_mgmt_name = local.cft_api_mgmt_name
   api_mgmt_rg   = local.cft_api_mgmt_rg
+  product_access_control_groups = ["developers"]
   providers = {
     azurerm = azurerm.aks-cftapps
   }
