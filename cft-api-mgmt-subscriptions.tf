39d6c34b7d31ab05c0af3e05503fdd6aac926166
David Jones <dave.jones@hmcts.net>
2024-07-26 20:37:02 +0100

    Update API Policy, subscriptions and thumbprints.

--- cft-api-mgmt-subscriptions.tf
+++ cft-api-mgmt-subscriptions.tf
@@ -17,7 +17,7 @@ resource "azurerm_key_vault_secret" "fee_pay_team_payment_subscription_key" {
 }
 
 # Supplier subscription - Liberata
-resource "azurerm_api_management_subscription" "liberata_supplier_subscription" {
+resource "azurerm_api_management_subscription" "liberata_supplier_payment_subscription" {
   api_management_name = local.cft_api_mgmt_name
   resource_group_name = local.cft_api_mgmt_rg
   product_id          = module.cft_api_mgmt_product.id
@@ -26,8 +26,8 @@ resource "azurerm_api_management_subscription" "liberata_supplier_subscription" 
   provider            = azurerm.aks-cftapps
 }
 
-resource "azurerm_key_vault_secret" "liberata_supplier_subscription_key" {
-  name         = "liberata-cft-apim-subscription-key"
-  value        = azurerm_api_management_subscription.liberata_supplier_subscription.primary_key
+resource "azurerm_key_vault_secret" "liberata_supplier_payment_subscription_key" {
+  name         = "liberata-cft-apim-payment-subscription-key"
+  value        = azurerm_api_management_subscription.liberata_supplier_payment_subscription.primary_key
   key_vault_id = data.azurerm_key_vault.payment_key_vault.id
 }
