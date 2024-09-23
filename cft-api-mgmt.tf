219fbb4ec45d87802a97dfe7a8d232465acb4dd5
Dave Jones <38425925+davejones74@users.noreply.github.com>
2024-09-23 11:30:31 +0100

    Remove legacy APIM Payment Config (#131)
    * Remove old APIM Payment Product.
    
    * Updating Terraform Formatting
    
    * Tidy up provider file.
    
    * Tidy up provider file.
    
    * Tidy up provider file.
    
    * Updating Terraform Formatting
    
    * Tidy up provider file.
    
    ---------
    
    Co-authored-by: 58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- cft-api-mgmt.tf
+++ cft-api-mgmt.tf
@@ -2,6 +2,7 @@ locals {
   cft_api_mgmt_suffix = var.apim_suffix == "" ? var.env : var.apim_suffix
   cft_api_mgmt_name   = join("-", ["cft-api-mgmt", local.cft_api_mgmt_suffix])
   cft_api_mgmt_rg     = join("-", ["cft", var.env, "network-rg"])
+  cft_api_base_path   = "payments-api"
 }
 
 provider "azurerm" {
@@ -38,7 +39,7 @@ module "cft_api_mgmt_api" {
   api_mgmt_name = local.cft_api_mgmt_name
   api_mgmt_rg   = local.cft_api_mgmt_rg
   product_id    = module.cft_api_mgmt_product.product_id
-  path          = local.api_base_path
+  path          = local.cft_api_base_path
   service_url   = local.payments_api_url
   swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.recon-payments-v0.3.json"
   protocols     = ["http", "https"]
--- main.tf
+++ main.tf
@@ -4,13 +4,9 @@ provider "azurerm" {
 }
 
 locals {
-  api_mgmt_name     = join("-", ["core-api-mgmt", var.env])
-  api_mgmt_rg       = join("-", ["core-infra", var.env])
   payment_key_vault = join("-", ["ccpay", var.env])
-  api_base_path     = "payments-api"
-
-  payments_api_url = join("", ["http://payment-api-", var.env, ".service.core-compute-", var.env, ".internal"])
-  s2sUrl           = join("", ["http://rpe-service-auth-provider-", var.env, ".service.core-compute-", var.env, ".internal"])
+  payments_api_url  = join("", ["http://payment-api-", var.env, ".service.core-compute-", var.env, ".internal"])
+  s2sUrl            = join("", ["http://rpe-service-auth-provider-", var.env, ".service.core-compute-", var.env, ".internal"])
 
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
   thumbprints_in_quotes     = formatlist("\"%s\"", var.api_gateway_test_certificate_thumbprints)
@@ -30,43 +26,4 @@ data "azurerm_key_vault_secret" "s2s_client_secret" {
 data "azurerm_key_vault_secret" "s2s_client_id" {
   name         = "gateway-s2s-client-id"
   key_vault_id = data.azurerm_key_vault.payment_key_vault.id
-}
-
-module "api_mgmt_product" {
-  source        = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
-  name          = var.product_name
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
-}
-
-module "api_mgmt_api" {
-  source        = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
-  name          = join("-", [var.product_name, "api"])
-  display_name  = "Payments API"
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
-  product_id    = module.api_mgmt_product.product_id
-  path          = local.api_base_path
-  service_url   = local.payments_api_url
-  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.recon-payments-v0.3.json"
-  revision      = "1"
-}
-
-data "template_file" "policy_template" {
-  template = file(join("", [path.module, "/template/api-policy.xml"]))
-
-  vars = {
-    allowed_certificate_thumbprints = local.thumbprints_in_quotes_str
-    s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
-    s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
-    s2s_base_url                    = local.s2sUrl
-  }
-}
-
-module "api_mgmt_policy" {
-  source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
-  api_mgmt_name          = local.api_mgmt_name
-  api_mgmt_rg            = local.api_mgmt_rg
-  api_name               = module.api_mgmt_api.name
-  api_policy_xml_content = data.template_file.policy_template.rendered
 }
